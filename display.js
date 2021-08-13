15bdec4dd787fa9c8572b174f9d4a89542fcffdd
mariordgez <mariordgez@hotmail.com>
2021-08-12 17:53:15 -0700

    Fix linter errors

--- display.js
+++ display.js
@@ -1,25 +1,29 @@
-
 export default class DisplayContent {
   static render(id) {
-    console.log("i GOT HERE")
-    document.querySelectorAll(".page-content").forEach((section) => {
-      section.setAttribute("style", "display: none");
-    })
+    document.querySelectorAll('.page-content').forEach((section) => {
+      section.setAttribute('style', 'display: none');
+    });
 
-    if (id === "list") {
-      document.querySelector(".book-list").setAttribute("style", "display: block");
+    if (id === 'list') {
+      document
+        .querySelector('.book-list')
+        .setAttribute('style', 'display: block');
     }
 
-    if (id === "addNew") {
-      document.querySelector(".book-form").setAttribute("style", "display: block");
+    if (id === 'addNew') {
+      document
+        .querySelector('.book-form')
+        .setAttribute('style', 'display: block');
     }
 
-    if (id === "contact") {
-      document.querySelector(".contact-info").setAttribute("style", "display: block");
+    if (id === 'contact') {
+      document
+        .querySelector('.contact-info')
+        .setAttribute('style', 'display: block');
     }
   }
 
-  static renderTime(dateandtime){
+  static renderTime(dateandtime) {
     document.querySelector('#dateAndTime').innerText = dateandtime;
   }
 }
--- index.js
+++ index.js
@@ -4,13 +4,21 @@ import UI from './ui.js';
 import DisplayContent from './display.js';
 
 const bookForm = document.querySelector('form');
-const navLinks = document.querySelectorAll('.nav')
-var DateTime = luxon.DateTime;
-const placeTime =  DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+const navLinks = document.querySelectorAll('.nav');
+const { luxon } = window;
+const { DateTime } = luxon;
 
-document.addEventListener('DOMContentLoaded', UI.displayList(Storage.getList().list));
-document.addEventListener('DOMContentLoaded', DisplayContent.renderTime(placeTime));
+const placeTime = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
 
+document.addEventListener(
+  'DOMContentLoaded',
+  UI.displayList(Storage.getList().list),
+);
+
+document.addEventListener(
+  'DOMContentLoaded',
+  DisplayContent.renderTime(placeTime),
+);
 
 bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -21,15 +29,15 @@ bookForm.addEventListener('submit', (e) => {
   Storage.saveBook(newBook);
   UI.addToUI(newBook);
   UI.deleteBook();
-  DisplayContent.render("list");
-  document.querySelector('.active-nav').classList.remove("active-nav");
-  document.querySelector("#list").classList.add("active-nav");
+  DisplayContent.render('list');
+  document.querySelector('.active-nav').classList.remove('active-nav');
+  document.querySelector('#list').classList.add('active-nav');
 });
 
 navLinks.forEach((nav) => {
   nav.addEventListener('click', (e) => {
-    document.querySelector('.active-nav').classList.remove("active-nav");
-    e.target.classList.add("active-nav");
-    DisplayContent.render(e.target.id)
-  })
-})
+    document.querySelector('.active-nav').classList.remove('active-nav');
+    e.target.classList.add('active-nav');
+    DisplayContent.render(e.target.id);
+  });
+});
